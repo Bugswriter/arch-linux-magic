948da87b7343bdf8efbfa6678f89ab03028a64e1
bugswriter <surajkushwah6969@gmail.com>
2020-11-26 12:08:24 +0530

    Adding my installation script

---
+++ arch_install.sh
@@ -0,0 +1,62 @@
+#Script 1
+echo "Welcome to Arch Linux Magic Script"
+pacman --noconfirm -Sy archlinux-keyring
+loadkeys us
+timedatectl set-ntp true
+echo "Enter the drive: "
+read drive
+cfdisk $drive 
+echo "Enter the linux partition: "
+read partition
+mkfs.ext4 $partition 
+read -p "Did you also created efi partition? [yn]" answer
+
+if [[ $answer = y ]] ; then
+  echo "Enter EFI partition: "
+  read efipartition
+  mkfs.vfat -F 32 $efipartition
+fi
+
+mount $partition /mnt 
+pacstrap /mnt base base-devel linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+
+sed '1,/^#part2$/d' arch_install.sh > /mnt/arch_install2.sh
+chmod +x /mnt/arch_install2.sh
+arch-chroot /mnt ./smas2.sh 
+exit 
+
+#part2
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "KEYMAP=us" > /etc/vconsole.conf
+echo "Hostname: "
+read hostname
+echo $hostname > /etc/hostname
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "::1             localhost" >> /etc/hosts
+echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
+mkinitcpio -P
+passwd
+pacman --noconfirm -S grub efibootmgr os-prober
+echo "Enter EFI partition: " 
+read efipartition
+mkdir /boot/efi
+mount $efipartition /boot/efi 
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman --noconfirm -S pulseaudio pulseaudio-alsa alsa-utils xorg xorg-apps xclip xwallpaper xorg-xinit dmenu firefox vim vi nano wget dhcpcd xdg-user-dirs zsh networkmanager
+systemctl enable NetworkManager.service 
+visudo
+echo "Enter Username: "
+read username
+useradd -m -G wheel -s /bin/bash $username
+passwd $username
+echo "Pre-Installation Finish Reboot now"
+
+
+
+ 
