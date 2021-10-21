f8d85fb4a219099630ea7cd3727e56cdfac0b82a
Suraj Kushwah <suraj@bugswriter.com>
2021-10-22 03:47:29 +0530

    all in one

--- arch_install.sh
+++ arch_install.sh
@@ -1,4 +1,5 @@
-#Script 1
+# == MY ARCH SETUP INSTALLER == #
+#part1
 echo "Welcome to Arch Linux Magic Script"
 pacman --noconfirm -Sy archlinux-keyring
 loadkeys us
@@ -11,17 +12,14 @@ echo "Enter the linux partition: "
 read partition
 mkfs.ext4 $partition 
 read -p "Did you also create efi partition? [y/n]" answer
-
 if [[ $answer = y ]] ; then
   echo "Enter EFI partition: "
   read efipartition
   mkfs.vfat -F 32 $efipartition
 fi
-
 mount $partition /mnt 
 pacstrap /mnt base base-devel linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-
 sed '1,/^#part2$/d' arch_install.sh > /mnt/arch_install2.sh
 chmod +x /mnt/arch_install2.sh
 arch-chroot /mnt ./arch_install2.sh
@@ -50,20 +48,48 @@ mount $efipartition /boot/efi
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop \
-     noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels \
+     noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels ttf-font-awesome \
      sxiv mpv zathura zathura-pdf-mupdf ffmpeg imagemagick  \
      fzf man-db xwallpaper python-pywal youtube-dl unclutter xclip maim \
      zip unzip unrar p7zip xdotool papirus-icon-theme \
      dosfstools ntfs-3g git sxhkd zsh \
      vim emacs arc-gtk-theme rsync \
      xcompmgr libnotify dunst slock \
-     dhcpcd networkmanager rsync
+     dhcpcd networkmanager rsync pamixer
 
 systemctl enable NetworkManager.service 
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
 echo "Enter Username: "
 read username
 useradd -m -G wheel -s /bin/zsh $username
 passwd $username
 echo "Pre-Installation Finish Reboot now"
+ai3_path=/home/$username/arch_install3.sh
+sed '1,/^#part3$/d' arch_install2.sh > $ai3_path
+chown $username:$username $ai3_path
+chmod +x $ai3_path
+su -c $ai3_path -s /bin/sh $username
+exit 
+
+#part3
+cd $HOME
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/bugswriter/dotfiles.git tmpdotfiles
+rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
+rm -r tmpdotfiles
+git clone --depth=1 https://github.com/Bugswriter/dwm.git ~/.local/src/dwm
+sudo make -C ~/.local/src/dwm install
+git clone --depth=1 https://github.com/Bugswriter/st.git ~/.local/src/st
+sudo make -C ~/.local/src/st install
+git clone --depth=1 https://github.com/Bugswriter/dmenu.git ~/.local/src/dmenu
+sudo make -C ~/.local/src/dmenu install
+git clone --depth=1 https://github.com/Bugswriter/baph.git ~/.local/src/baph
+sudo make -C ~/.local/src/baph install
+baph -inN libxft-bgra-git brave-bin 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ln -s ~/.config/x11/xinitrc .xinitrc
+ln -s ~/.config/shell/profile .zprofile
+
+mv ~/.oh-my-zsh ~/.config/zsh/oh-my-zsh
+rm ~/.zshrc ~/.zsh_history
+exit
