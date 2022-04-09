beae5746447e100d9cf8e782356f0ff7d5c849a3
Suraj Kushwah <suraj@bugswriter.com>
2022-04-10 01:38:46 +0530

    changing paru to pikaur

--- arch_install.sh
+++ arch_install.sh
@@ -61,8 +61,8 @@ pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xback
      fzf man-db xwallpaper python-pywal unclutter xclip maim \
      zip unzip unrar p7zip xdotool papirus-icon-theme brightnessctl  \
      dosfstools ntfs-3g git sxhkd zsh pipewire pipewire-pulse \
-     vim emacs arc-gtk-theme rsync firefox dash \
-     xcompmgr libnotify dunst slock jq aria2 \
+     emacs-nox arc-gtk-theme rsync qutebrowser dash \
+     xcompmgr libnotify dunst slock jq aria2 cowsay \
      dhcpcd networkmanager rsync pamixer mpd ncmpcpp \
      zsh-syntax-highlighting xdg-user-dirs
 
@@ -88,17 +88,33 @@ cd $HOME
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/bugswriter/dotfiles.git tmpdotfiles
 rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
 rm -r tmpdotfiles
+# dwm: Window Manager
 git clone --depth=1 https://github.com/Bugswriter/dwm.git ~/.local/src/dwm
 sudo make -C ~/.local/src/dwm install
+
+# st: Terminal
 git clone --depth=1 https://github.com/Bugswriter/st.git ~/.local/src/st
 sudo make -C ~/.local/src/st install
+
+# dmenu: Program Menu
 git clone --depth=1 https://github.com/Bugswriter/dmenu.git ~/.local/src/dmenu
 sudo make -C ~/.local/src/dmenu install
-git clone --depth=1  https://aur.archlinux.org/paru.git ~/.local/src/paru
-cd ~/.local/src/paru
-makepkg -si
+
+# dmenu: Dmenu based Password Prompt
+git clone --depth=1 https://github.com/ritze/pinentry-dmenu.git ~/.local/src/pinentry-dmenu
+sudo make -C ~/.local/src/pinentry-dmenu clean install
+
+# dwmblocks: Status bar for dwm
+git clone --depth=1 https://github.com/bugswriter/dwmblocks.git ~/.local/src/dwmblocks
+sudo make -C ~/.local/src/dwmblocks install
+
+# pikaur: AUR helper
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -fsri
 cd
-paru -S libxft-bgra-git yt-dlp-drop-in
+pikaur -S libxft-bgra-git yt-dlp-drop-in
+mkdir dl dox imp music pix pub code
 
 ln -s ~/.config/x11/xinitrc .xinitrc
 ln -s ~/.config/shell/profile .zprofile
